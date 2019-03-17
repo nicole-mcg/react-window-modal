57fc213d81c81ae9b50c48dc681ab8f2ce9e1c03
Nik <niknakbakpak@gmail.com>
2019-03-17 13:43:04 -0400

    update build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,57 @@
 const path = require("path");
+const fs = require("fs")
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-    entry: "",
-    entry: {
-      index: './src/index.tsx',
-    },
-    devtool: "inline-source-map",
-    mode: "production",
-    module: {
-      rules: [
-        {
-          test: /\.(tsx|ts)$/,
-          use: [
-            'ts-loader',
-            'source-map-loader',
-          ]
-        },
-        {
-          test: /\.(less|css)$/,
-          use: [
-            {
-              loader: 'css-loader' // translates CSS into CommonJS
-            },
-            {
-              loader: 'less-loader', // compiles Less to CSS
-              options: {
-                javascriptEnabled: true
-              }
-            },
-          ],
-        }
-      ]
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-      plugins: [
-        new TsconfigPathsPlugin(),
-      ]
-    },
-    output: {
-      filename: "[name].js",
-      path: path.resolve(__dirname, "build"),
-    },
+  entry: "",
+  entry: {
+    index: './src/index.tsx',
+  },
+  target: 'web',
+  devtool: "inline-source-map",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        use: [
+          'ts-loader',
+          'source-map-loader',
+        ]
+      },
+      {
+        test: /\.(less|css)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader' // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader', // compiles Less to CSS
+            options: {
+              javascriptEnabled: true
+            }
+          },
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    plugins: [
+      new TsconfigPathsPlugin(),
+    ]
+  },
+  output: {
+    library: "WindowModal",
+    libraryExport: "WindowModal",
+    libraryTarget: 'umd',
+    auxiliaryComment: 'Test Comment',
+    filename: "[name].js",
+    path: path.resolve(__dirname, "build"),
+  },
 };
